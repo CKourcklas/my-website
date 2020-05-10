95352fa64d2c9d28d188ae6614dcd7f52f8ea978
Cory Kourcklas <ckourcklas@gmail.com>
2020-05-10 12:37:11 -0400

    5/10 12:37 PM

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,12 +1,27 @@
 <template>
-  <div id="education" class="pb-20" style="background-color: #ffa50054;">
+  <div id="education" class="pb-20" style="background-color: rgba(255, 165, 0, 0.24);">
     <div class="mt-8 text-4xl md:text-6xl font-bold md:mt-16 text-center pt-12">Education</div>
     <div class="flex justify-center mt-12">
-      <div class="border-black p-6 md-border-8 border-black">
-        <div class="font-bold text-3xl">BACHELOR OF BUSINESS ADMINSTRATION</div>
+      <div
+        class="p-6"
+        style="background-color: #e2e8f0; text-align: center; border: 3px solid black;"
+      >
+        <div class="font-bold text-xl">BACHELOR OF BUSINESS ADMINISTRATION</div>
+        <div class="border-2 rounded-lg mt-3" style="border-color: #ed8936;"></div>
+        <div class="mt-3 text-4xl font-bold" style="color: #052371;">Marketing</div>
+        <div class="mt-3">The University of Akron</div>
+        <div>2013-2017</div>
       </div>
-      <!-- <div class="md:w-16"></div>
-      <div class="h-64 w-64 border-black border-4"></div> -->
+      <!-- <div
+        class="p-6"
+        style="background-color: #e2e8f0; text-align: center; border: 3px solid black;"
+      >
+        <div class="font-bold text-xl">BACHELOR OF BUSINESS ADMINISTRATION</div>
+        <div class="border-2 rounded-lg mt-3" style="border-color: #ed8936;"></div>
+        <div class="mt-3 text-4xl font-bold" style="color: #052371;">Marketing</div>
+        <div class="mt-3">The University of Akron</div>
+        <div>2013-2017</div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -21,7 +36,7 @@ export default {
 <style>
 @media (min-width: 768px) {
   .md-border-8 {
-    border-width: 8px;
+    border-width: 3px;
   }
 }
 </style>
