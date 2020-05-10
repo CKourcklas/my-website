d52bc13aceaa9486cc7df0d5b5bd159e1addca7c
Cory Kourcklas <ckourcklas@gmail.com>
2020-05-09 23:44:33 -0400

    5/9/2020 11:45 PM

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,9 +1,13 @@
 <template>
-  <div
-    id="education"
-    class="flex flex-col md:flex-row justify-center items-center bg-red-400 h-screen"
-  >
-    <span class="text-4xl">Education Section</span>
+  <div id="education" class="pb-20" style="background-color: #ffa50054;">
+    <div class="mt-8 text-4xl md:text-6xl font-bold md:mt-16 text-center pt-12">Education</div>
+    <div class="flex justify-center mt-12">
+      <div class="border-black p-6 md-border-8 border-black">
+        <div class="font-bold text-3xl">BACHELOR OF BUSINESS ADMINSTRATION</div>
+      </div>
+      <!-- <div class="md:w-16"></div>
+      <div class="h-64 w-64 border-black border-4"></div> -->
+    </div>
   </div>
 </template>
 
@@ -15,4 +19,9 @@ export default {
 </script>
 
 <style>
+@media (min-width: 768px) {
+  .md-border-8 {
+    border-width: 8px;
+  }
+}
 </style>
