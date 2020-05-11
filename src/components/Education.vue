038e7c00ee96439e6ecd8d7a2deca22063b2c6b0
Cory Kourcklas <ckourcklas@gmail.com>
2020-05-11 17:22:12 -0400

    5/11 5:22 PM

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="education" class="pb-20" style="background-color: rgba(255, 165, 0, 0.24);">
     <div class="mt-8 text-4xl md:text-6xl font-bold md:mt-16 text-center pt-12">Education</div>
-    <div class="flex justify-evenly mt-12">
+    <div class="flex flex-col md:flex-row justify-center items-center md:justify-evenly mt-12">
       <div
         v-bind:key="index"
         v-for="(education, index) in education"
-        class="p-6"
-        style="background-color: #e2e8f0; text-align: center; border: 3px solid black; width: 450px;"
+        class="p-6 text-center mt-5 md:mt-0"
+        style="background-color: #e2e8f0; border: 3px solid black; width: 450px;"
       >
         <div class="font-bold text-lg uppercase">{{education.degree}}</div>
         <div class="border-2 rounded-lg mt-3" style="border-color: #ed8936;"></div>
